4f7f355c22deba04db2365dc0e8e2ab768fd4386
Maria Coitinho <brasildu@gmail.com>
2022-11-10 01:17:03 -0600

    commit tree a8f8611c29b72a04a087022fdb98c357369277e7

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 23, "M", "brown", "calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 23, "M", "brown", "calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 30, "F", "black", "calm", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 44, "M", "brown", "calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 50, "F", "white", "angry", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 49, "M", "black", "sad", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 50, "F", "brown", "crazy", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 3, "M", "brown", "calm", 1);
